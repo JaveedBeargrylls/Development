f887867ccde3687a5dad12bb6c34ab1836451df3
JaveedBeargrylls <76989549+JaveedBeargrylls@users.noreply.github.com>
2021-08-25 18:07:46 +0530

    [Javeed] Add : Basic Core Programmes using Python

--- ReplaceString.py
+++ ReplaceString.py
@@ -12,4 +12,4 @@ Username = input('Enter Your Name :')
 if(re.findall('[A-za-z]{3,}',Username)): #ensuring the username contains min 3 characters
     print('Hello',Username,'How are you ?')
 else:
-    print("Enter the valid UserName")
+    print(" Enter the valid UserName ")
